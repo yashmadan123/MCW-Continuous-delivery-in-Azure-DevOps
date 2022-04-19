1c8e99a9ec6089571c2d0c94f6b8e469f113193c
Sumit Malik <45102602+sumitmalik51@users.noreply.github.com>
2022-04-19 15:15:19 +0530

    Update deploy-infrastructure.ps1

--- Hands-on lab/lab-files/infrastructure/deploy-infrastructure.ps1
+++ Hands-on lab/lab-files/infrastructure/deploy-infrastructure.ps1
@@ -6,11 +6,6 @@ $planName = "fabmedical-plan-" + $studentprefix
 $location1 = "westeurope"
 $location2 = "northeurope"
 
-# Create resource group
-az group create `
-    --location $location1 `
-    --name $resourcegroupName
-
 # Create Azure Cosmos DB database
 az cosmosdb create `
     --name $cosmosDBName `
